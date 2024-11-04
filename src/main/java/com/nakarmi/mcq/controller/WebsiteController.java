922c53152564502f2f6b9fe724b27ac0211d5704
JOSEPH NAKARMI <josephnkm@gmail.com>
2024-11-05 00:37:32 +0545

    added api for joseph website message

---
+++ src/main/java/com/nakarmi/mcq/controller/WebsiteController.java
@@ -0,0 +1,38 @@
+package com.nakarmi.mcq.controller;
+
+import com.nakarmi.mcq.entity.MessageDocument;
+import com.nakarmi.mcq.repository.MessageRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("josephnakarmi")
+public class WebsiteController {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @PostMapping("message")
+    public ResponseEntity<String> postMessage(@RequestBody Map<String, String> request) {
+        log.info("postMessage request: {}", request);
+        String message = request.get("message");
+        String name = request.get("name");
+        String email = request.get("email");
+
+        MessageDocument messageDocument = new MessageDocument();
+        messageDocument.setName(name);
+        messageDocument.setMessage(message);
+        messageDocument.setEmail(email);
+        messageRepository.save(messageDocument);
+        return ResponseEntity.ok("message submitted successfully");
+    }
+}
+
---
+++ src/main/java/com/nakarmi/mcq/entity/MessageDocument.java
@@ -0,0 +1,23 @@
+package com.nakarmi.mcq.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Document("messages")
+public class MessageDocument {
+    @Id
+    private String id;
+    private String name;
+    private String message;
+    private String email;
+
+
+}
